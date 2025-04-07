393ff228a58a3c3caa60661150526b34856bb6cf
JavierPs287 <javplazasanchez@gmail.com>
2025-04-07 18:16:26 +0200

    .

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -4,19 +4,19 @@ import Ice
 import RemoteCalculator as rc
 
 class Calculator(rc.Calculator):
-    def sum(a,b,current=None):
+    def sum(a,b,current):
         """Sum two numbers."""
         return a + b
     
-    def sub(a,b,current=None):  
+    def sub(a,b,current):  
         """Subtract two numbers."""
         return a - b
     
-    def mult(a,b,current=None):
+    def mult(a,b,current):
         """Multiply two numbers."""
         return a * b
     
-    def div(a,b,current=None):
+    def div(a,b,current):
         """Divide two numbers."""
         if b == 0:
             raise ZeroDivisionError("Division by zero is not allowed.")
