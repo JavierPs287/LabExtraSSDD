cb3706bb7a40af77e863696658f4b3eb6bfe7720
JavierPs287 <javplazasanchez@gmail.com>
2025-06-08 12:02:32 +0200

    Código Comentado

--- cliente_kafka/consumer_cliente.py
+++ cliente_kafka/consumer_cliente.py
@@ -1,6 +1,7 @@
 import json
 import confluent_kafka as ck
 
+# Creamos el consumer que recibe la respuesta
 def generar_consumer():
     config = {
         "bootstrap.servers": "localhost:9092",
@@ -9,12 +10,14 @@ def generar_consumer():
     }
     return ck.Consumer(config)
 
+# Metodo que espera un mensaje del topic "resultados" y devuelve el resultado
 def esperar_mensaje(id):
     consumer = generar_consumer()
     consumer.subscribe(["resultados"])
     print("Esperando mensaje...")
 
     while True:
+        # Bucle que espera un mensaje
         mensajes = consumer.consume(num_messages=1, timeout=1.0)
 
         if not mensajes:
@@ -27,10 +30,13 @@ def esperar_mensaje(id):
             continue
 
         try:
+            # Decodifica el mensaje y verifica el ID
             data = json.loads(mensaje.value().decode("utf-8"))
             if data["id"] == id:
+                # Si el ID coincide, devolvemos el resultado
                 return data
             else:
+                # Si el ID no coincide, seguimos esperando
                 print("ID no coincide. Esperando el mensaje correcto...")
         except json.JSONDecodeError:
             print("Error decoding JSON")
--- cliente_kafka/producer_cliente.py
+++ cliente_kafka/producer_cliente.py
@@ -4,10 +4,12 @@ import uuid
 import consumer_cliente as cc
 import time
 
+# Metodo que se encarga de generar el productor de Kafka
 def generar_producer():
     config = {"bootstrap.servers": "localhost:9092"}
     return ck.Producer(config)
 
+# Metodo que muestra el menú de operaciones disponibles
 def mostrar_menu():
     print("\nCalculadora remota - Elige una operación:")
     print("1. Suma")
@@ -26,6 +28,7 @@ def mostrar_menu():
         except ValueError:
             print("Entrada inválida. Introduce un número.")
 
+# Metodo que recoge los operandos de la operación
 def obtener_operandos():
     while True:
         try:
@@ -35,27 +38,29 @@ def obtener_operandos():
         except ValueError:
             print("Por favor, introduce números válidos.")
 
+# Metodo principal que ejecuta la aplicación
 def main():
     producer = generar_producer()
     topic = "calculadora"  # Topic donde se enviarán las solicitudes
     n = 0
     
     while True:
+        # Se muestra el menu
         opcion = mostrar_menu()
         
         if opcion == 5:
             print("Saliendo...")
             break
         
+        # Se asigna la operación elegida y los operandos
         operaciones = {1: "sum", 2: "sub", 3: "mult", 4: "div"}
-        operacion = operaciones[opcion]
-        
+        operacion = operaciones[opcion]        
         print(f"\nOperación seleccionada: {operacion}")
         op1, op2 = obtener_operandos()
         
         # Generar un ID único para esta operación
         n+=1
-        operacion_id = str(f"{operacion}{n}") #Cambiar id
+        operacion_id = str(f"{operacion}{n}")
         
         # Construir el mensaje
         mensaje = {
@@ -70,8 +75,9 @@ def main():
         # Enviar el mensaje
         producer.produce(topic, value=json.dumps(mensaje).encode('utf-8'))
         producer.flush()
-        
         print(f"\nSolicitud enviada con ID: {operacion_id}")
+
+        # Esperamos la respuesta
         print(f"Esperando resultado para la operación: {op1} {operacion} {op2}...\n")
         data=cc.esperar_mensaje(operacion_id)
         print("Respuesta recibida.\n")
